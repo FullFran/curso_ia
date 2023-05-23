80954cbf1a4bdf35dc7b9890c53f9f695a150f3b
FullFran <franfi17sw@gmail.com>
2023-05-23 11:17:21 +0200

    añadidas correcciones de la clase 5

--- clase4/classvector.py
+++ clase4/classvector.py
@@ -14,19 +14,47 @@ Ejemplo de archivo:
 Nota: acordarse de que en el método de escribir en un archivo y en esta función hay que poner formatos de string compatibles.
 '''
 
+# Vamos a definir también la suma de vectores.
+
 
 class vector():
     def __init__(self, x, y, z):
-        self.__x = x
-        self.__y = y
-        self.__z = z
+        self.__x = float(x)
+        self.__y = float(y)   # asi nos aseguramos de meterle un float
+        self.__z = float(z)
+
+    # Overwrite de operadores
+    def __add__(self, v):
+        new_vector = vector(0, 0, 0)
+        new_vector.__x = float(self.__x + v.__x)
+        new_vector.__y = float(self.__y + v.__y)
+        new_vector.__z = float(self.__z + v.__z)
+        return (new_vector)
+
+    def __sub__(self, v):
+        new_vector = vector(0, 0, 0)
+        new_vector.__x = float(self.__x - v.__x)
+        new_vector.__y = float(self.__y - v.__y)
+        new_vector.__z = float(self.__z - v.__z)
+        return new_vector
+
+    def __mul__(self, v):
+        new_vector = vector(0, 0, 0)
+
+        if isinstance(v, float) or isinstance(v, int):
+            new_vector.__x = float(self.__x * v)
+            new_vector.__y = float(self.__y * v)
+            new_vector.__z = float(self.__z * v)
+            return (new_vector)
+        elif isinstance(v, vector):
+            return self.__x * v.__x + self.__y * v.__y + self.__z * v.__z
 
     def imprimir_vector(self):
-        print(f'({self.__x},{self.__y},{self.__z})')
+        return f'({self.__x},{self.__y},{self.__z})'
 
     def meter_txt(self, archivo):
         with open(archivo, 'a') as f:
-            f.write(f'({self.__x},{self.__y},{self.__z})\n')
+            f.write(f'{self.imprimir_vector()}\n')
         # acordarse de que hay que poner formatos compatibles.
 
 
@@ -42,12 +70,14 @@ def hacer_vector(archivo):
                 vs.append(v)
     return vs
 
+# ---------Lo Probamos----------------------------------------------
+
 
 print('Creamos vectores e imprimimos el primero.')
 v1 = vector(1, 1, 0)
 v2 = vector(0, 0, 1)
 v3 = vector(-1, 1, 0)
-v1.imprimir_vector()
+print(v1.imprimir_vector())
 
 print('\nAñadimos vectores a un .txt')
 v1.meter_txt('clase4/vecs.txt')
@@ -58,4 +88,22 @@ print('\nCargamos los vectores desde un archivo')
 vectores = hacer_vector('clase4/baseortonormal.txt')
 
 for v in vectores:
-    v.imprimir_vector()
+    print(v.imprimir_vector())
+
+
+print('\nVamos a sumar vectores')
+print((v1.__add__(v2)).imprimir_vector())
+print((v1+v2).imprimir_vector())
+
+print('\nVamos a restar vectores')
+print((v1.__sub__(v2)).imprimir_vector())
+print((v1-v2).imprimir_vector())
+
+print('\nVamos a multiplicar vectores escalarmente')
+# Hemos puesto esto porque los hice linealmente independientes XD
+print(((v1+v3).__mul__(v3)))
+print((v1+v3)*v3)
+
+print('\nVamos a multiplicar vectores por un escalar')
+print((v1.__mul__(2)).imprimir_vector())
+print((v1*2).imprimir_vector())
